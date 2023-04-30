57d423a82373a368d25f3cd3fba361672b7f8b24
Junhyuk Park <junhyeg08@gmail.com>
2023-04-30 21:41:49 +0900

    feat: typescript 기본 구성

---
+++ 02/client.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import WordRelayClass from "./WordRelayClass";
+
+ReactDOM.render(<WordRelayClass />, document.querySelector("#root"));
