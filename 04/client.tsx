fc3ef120c0fba928a1ad0e9df3acfd10ef87c698
Junhyuk Park <junhyeg08@gmail.com>
2023-05-01 19:27:38 +0900

    feat: state, ref type 복습

---
+++ 04/client.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import ResponseCheckClass from "./ResponseCheckClass";
+
+ReactDOM.render(<ResponseCheckClass />, document.querySelector("#root"));
